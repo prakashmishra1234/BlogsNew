1c231af6d1dece9b8987e840d482afe471fae751
Prakash Mishra <prakash@sflhub.com>
2022-05-31 12:38:04 +0530

    Added procfile

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,6 @@ dotenv.config({ path: "backend/config/config.env" });
 //connecting to database
 connectDataBase();
 
-app.listen(process.env.PATH||8080, () => {
-  console.log(`server working on http://localhost:${process.env.PATH}`);
+app.listen(process.env.PATH || 8080, () => {
+  console.log(`server is working `);
 });
