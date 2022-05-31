5390aa06256b00791e64e62ae6c695674c279dcf
Prakash Mishra <prakash@sflhub.com>
2022-05-31 14:02:29 +0530

    Changes in server.js file

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,6 @@ dotenv.config({ path: "backend/config/config.env" });
 //connecting to database
 connectDataBase();
 
-app.listen(process.env.PATH || 8080, () => {
+app.listen(process.env.PORT, () => {
   console.log(`server is running `);
 });
